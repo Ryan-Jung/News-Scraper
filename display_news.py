58a3388b35e6336e3f59cc784389fd4c411fc700
Ryan <ryanjung93@gmail.com>
2016-11-04 22:57:56 -0700

    Added status bar

--- display_news.py
+++ display_news.py
@@ -1,6 +1,7 @@
 import news_scraper
 import wx
 import webbrowser
+import time
 #Define GUI
 class NewsWindow(wx.Frame):
 
@@ -11,20 +12,23 @@ class NewsWindow(wx.Frame):
 		self.currentUserSelection = 0
 		self.mainsizer = wx.BoxSizer(wx.VERTICAL)
 		self.listbox = None
-
+		
+		self.refresh_button = wx.Button(self, label ="Refresh")
+		self.view_button = wx.Button(self, label = "View")
+		
+		self.statusBar = self.CreateStatusBar()
+		self.statusBar.SetStatusText("Waiting for your selection...")
 		#Handles article display
 		self.updateListBox()
 
 		#Add widgets to be displayed
-		self.refresh_button = wx.Button(self, label ="Refresh")
 		self.mainsizer.AddStretchSpacer()
 		self.mainsizer.Add(self.refresh_button)
 		self.mainsizer.Add(self.listbox)
-		self.view_button = wx.Button(self, label = "View")
 		self.mainsizer.AddStretchSpacer()
 		self.mainsizer.Add(self.view_button, flag=wx.CENTER)
-		self.mainsizer.AddStretchSpacer()	
-		
+		self.mainsizer.Add(self.statusBar)
+
 		#Event handling
 		self.Bind(wx.EVT_LISTBOX, self.saveCurrentSelection)
 		self.Bind(wx.EVT_BUTTON, self.onClick, self.view_button)
@@ -47,20 +51,30 @@ class NewsWindow(wx.Frame):
 
 	def onClick(self, event):
 		"""Opens default browser to display URL of the user's selected article
+		updates status bar to inform user of decision
 		"""
-		index = self.currentUserSelection
-		urls = self.data["urls"]
-		webbrowser.open(urls[index], 0, autoraise=True)
+		if self.data is not None:
+			index = self.currentUserSelection
+			urls = self.data["urls"]
+			articles = self.data["articles"]
+			self.statusBar.SetStatusText("Opening: " + articles[index] + " ...")
+			webbrowser.open(urls[index], 0, autoraise=True)
+			#Give time for browser to open
+			time.sleep(5)
+			self.statusBar.SetStatusText("Finished opening: " + articles[index])
 
 	def saveCurrentSelection(self, listbox_event):
-		"""Keeps track of the user's selection
+		"""Keeps track of the user's selection 
 		"""
 		self.currentUserSelection = listbox_event.GetSelection()
+
 	
 	def onRefresh(self,event):
 		"""Displays the current news articles available on www.sfgate.com
 		"""
+		self.statusBar.SetStatusText("Gathering latest articles...")
 		self.updateListBox()
+		self.statusBar.SetStatusText("Finished!")
 		#self.SetSizerAndFit(self.mainsizer)
 
 		
@@ -69,7 +83,7 @@ class NewsWindow(wx.Frame):
 		"""Creates listbox that holds all the articles and allows user interaction with 
 		items in the listbox. 
 		"""
-		if not self.listbox:
+		if self.listbox is None:
 			self.listbox = wx.ListBox(self)
 			self.listbox.SetMinSize(self.GetSize())
 
@@ -77,7 +91,7 @@ class NewsWindow(wx.Frame):
 		if self.data is not None:
 			self.listbox.Set(self.data["articles"])
 		else:
-			self.listbox.Set(["No Articles to display"])	
+			self.statusBar.SetStatusText("No articles to display")
 
        
 app = wx.App(False)
